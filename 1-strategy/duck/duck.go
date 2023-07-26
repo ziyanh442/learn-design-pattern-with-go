a31d12ac202f36388df7bd050173257328977f75
ziyanh442 <ziyanh42@gmail.com>
2023-07-26 17:49:28 +0700

    use interface

--- 1-strategy/duck/duck.go
+++ 1-strategy/duck/duck.go
@@ -1,9 +1,23 @@
 package duck
 
+type DuckInterface interface {
+	Quack()
+	Fly()
+	Display()
+}
+
 type Duck struct {
 	QuackBehaviour   func()
 	FlyBehaviour     func()
 	DisplayBehaviour func()
+}
+
+func NewDuck(quackBehaviour func(), flyBehaviour func(), displayBehaviour func()) DuckInterface {
+	return &Duck{
+		QuackBehaviour:   quackBehaviour,
+		FlyBehaviour:     flyBehaviour,
+		DisplayBehaviour: displayBehaviour,
+	}
 }
 
 func (d *Duck) Quack() {
--- 1-strategy/main.go
+++ 1-strategy/main.go
@@ -6,33 +6,21 @@ import (
 )
 
 func main() {
-	normalDuck := duck.Duck{
-		QuackBehaviour:   duck.NormalQuack,
-		FlyBehaviour:     duck.NormalFly,
-		DisplayBehaviour: duck.NormalDisplay,
-	}
+	normalDuck := duck.NewDuck(duck.NormalQuack, duck.NormalFly, duck.NormalDisplay)
 	normalDuck.Display()
 	normalDuck.Quack()
 	normalDuck.Fly()
 
 	fmt.Println("======================")
 
-	rubberDuck := duck.Duck{
-		QuackBehaviour:   duck.MuteQuack,
-		FlyBehaviour:     duck.NotMoving,
-		DisplayBehaviour: duck.RubberDisplay,
-	}
+	rubberDuck := duck.NewDuck(duck.MuteQuack, duck.NotMoving, duck.RubberDisplay)
 	rubberDuck.Display()
 	rubberDuck.Quack()
 	rubberDuck.Fly()
 
 	fmt.Println("======================")
 
-	injuredDuck := duck.Duck{
-		QuackBehaviour:   duck.NormalQuack,
-		FlyBehaviour:     duck.AtLeastItTried,
-		DisplayBehaviour: duck.InjuredDisplay,
-	}
+	injuredDuck := duck.NewDuck(duck.NormalQuack, duck.AtLeastItTried, duck.InjuredDisplay)
 	injuredDuck.Display()
 	injuredDuck.Quack()
 	injuredDuck.Fly()
